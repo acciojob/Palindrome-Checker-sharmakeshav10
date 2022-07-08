afe2802ede32c99908d83ef9ff721a407905e50f
Keshav Sharma <sharmakeshav0708@gmail.com>
2022-07-08 16:17:10 +0530

    check palindrome

--- solution.js
+++ solution.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
+    let newStr = str.toLowerCase();
+    let reverse = newStr.split('').reverse().join('');
 
+    return newStr === reverse;
 }
+
+palindrome(str);
+
 module.exports = palindrome
